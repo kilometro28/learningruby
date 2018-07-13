b9e2754c9e34597bfb067d75b12fb9ea82e7e7fa
kilometro28 <venturaguc@gmail.com>
2018-07-13 22:13:51 +0200

    updating new files

---
+++ 20_classes/1_private_methods.rb
@@ -0,0 +1,33 @@
+# Private methods
+class Gadget
+
+  attr_accessor :username
+  attr_reader :production_number
+  attr_writer :password
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @production_number = generate_production_number
+  end
+
+  def to_s
+    "Gadget #{@production_number} has the username #{@username}.
+    It is made from the #{self.class} class and it
+    has the ID #{self.object_id}"
+  end
+
+  private
+
+  def generate_production_number
+    start_digits = rand(10000..99999)
+    end_digits = rand(10000..99999)
+    alphabet = ("A".."B").to_a
+    middle_digits = "2018"
+    5.times { middle_digits << alphabet.sample }
+    "#{start_digits}-#{middle_digits}-#{end_digits}"
+  end
+end
+
+phone = Gadget.new("user", "password")
+p phone.production_number
